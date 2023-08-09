081e000be80322266a55406b5b569ce664e43868
Weronika Gaweł <126601389+GWeronika@users.noreply.github.com>
2023-08-09 14:58:50 +0200

    Add files via upload

---
+++ SkierowanieLekarzaBadania.java
@@ -0,0 +1,103 @@
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.sql.ResultSet;
+
+public class SkierowanieLekarzaBadania extends JFrame implements BazaDanychConnect {
+    public SkierowanieLekarzaBadania(int idLekarza) {
+        super("Panel lekarza");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(900, 600);
+        setLocationRelativeTo(null);
+
+        //tworzenie głównego kontenera JPanel
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(15, 30, 15, 30));
+
+        //panel przycisku i przycisk
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        JButton button = new JButton("+ Dodaj nowe skierowanie");
+
+        button.addActionListener(e -> {
+            TworzenieSkierowaniaBadania noweSkierowanieFrame = new TworzenieSkierowaniaBadania(idLekarza);
+            noweSkierowanieFrame.setVisible(true);
+            setVisible(false);
+        });
+
+        button.setFocusPainted(false);
+        button.setPreferredSize(new Dimension(200, 30));
+        buttonPanel.add(button);
+        buttonPanel.setBorder(BorderFactory.createEmptyBorder(0,0,0,-5));
+
+        String[] columnNames = {"Imie", "Nazwisko", "Data urodzenia", "PESEL", "Płeć", "nr Telefonu", "Cel skierowania"};
+
+        //pusty model
+        DefaultTableModel model = new DefaultTableModel();
+
+        for (String columnName : columnNames) {
+            model.addColumn(columnName);
+        }
+
+        JTable table = new JTable(model);
+
+        //pobieranie danych do tabeli
+        try {
+            String query = "SELECT pacjent.imie, pacjent.nazwisko, data_urodzenia, pesel, plec, nrTelefonu, skierowaniebadanie.nazwa_badania AS celSkierowania " +
+                    "FROM pacjent JOIN skierowaniebadanie ON skierowaniebadanie.idPacjenta = pacjent.id JOIN pracownik ON skierowaniebadanie.idLekarza = pracownik.id " +
+                    "WHERE pracownik.id = " + idLekarza;
+            ResultSet resultSet = polaczBaze(query);
+
+            //dodanie danych do modelu tabeli
+            while (resultSet.next()) {
+                String name = resultSet.getString("imie");
+                String surname = resultSet.getString("nazwisko");
+                String birthD = resultSet.getString("data_urodzenia");
+                String pesel = resultSet.getString("pesel");
+                String gender = resultSet.getString("plec");
+                String tel = resultSet.getString("nrTelefonu");
+                String spec = resultSet.getString("celSkierowania");
+
+                Object[] rowData = {name, surname, birthD, pesel, gender, tel, spec};
+                model.addRow(rowData);
+            }
+
+            resultSet.close();
+        } catch (Exception e) {
+            System.out.println("Blad pobieranie danych z tabeli");
+        }
+        //ustawienie preferowanego rozmiaru kolumn
+        table.getColumnModel().getColumn(0).setPreferredWidth(100);
+        table.getColumnModel().getColumn(1).setPreferredWidth(100);
+        table.getColumnModel().getColumn(2).setPreferredWidth(100);
+        table.getColumnModel().getColumn(3).setPreferredWidth(150);         //pesel
+        table.getColumnModel().getColumn(4).setPreferredWidth(50);          //płeć
+        table.getColumnModel().getColumn(5).setPreferredWidth(100);
+        table.getColumnModel().getColumn(6).setPreferredWidth(150);         //cel skierowania
+
+
+        JPanel tablePanel = new JPanel(new BorderLayout());
+        tablePanel.add(new JScrollPane(table), BorderLayout.CENTER);
+
+        //tworzenie panelu na przycisk powrotu
+        JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+        //dodanie przycisku Powrót do panelu na dole
+        JButton goBackButton = new JButton("Powrót");
+        bottomPanel.add(goBackButton);
+        goBackButton.addActionListener(e -> {
+            FrameLekarza lekarzFrame = new FrameLekarza(idLekarza);
+            lekarzFrame.setVisible(true);
+            setVisible(false);
+        });
+        bottomPanel.setBorder(BorderFactory.createEmptyBorder(0,-5,0,0));
+
+
+        //dodanie paneli do panelu głównego
+        mainPanel.add(buttonPanel, BorderLayout.NORTH);
+        mainPanel.add(tablePanel, BorderLayout.CENTER);
+        mainPanel.add(bottomPanel, BorderLayout.SOUTH);
+
+        add(mainPanel);
+    }
+}
